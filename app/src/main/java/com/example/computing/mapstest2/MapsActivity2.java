217f02b308e2a3d0e50ad76cb9fa2b6e0d0ff8bf
Max Cussans <max-cussans2@hotmail.co.uk>
2016-12-20 11:05:49 +0000

    Postcode Search working

--- app/src/main/java/com/example/computing/mapstest2/MapsActivity2.java
+++ app/src/main/java/com/example/computing/mapstest2/MapsActivity2.java
@@ -1,13 +1,21 @@
 package com.example.computing.mapstest2;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
 
@@ -22,13 +30,36 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
+    public void onSearch(View view)
+    {
+        EditText search_Address = (EditText)findViewById(R.id.searchAddress);
+        String location = search_Address.getText().toString();
+        List<Address> addressList = null;
+
+        if(location == null || location.equals(""))
+        {
+        }
+        else
+        {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+               addressList = geocoder.getFromLocationName(location, 1);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng addressLoc = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(addressLoc).title("Marker"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(addressLoc, 14.0f));
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMyLocationEnabled(true);
     }
 }
